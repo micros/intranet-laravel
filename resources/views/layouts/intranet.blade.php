45018e3b1e5533f036f4d00b703f04b7f5b8f690
Leonardo Forero Sandoval <gerencia@micrositios.net>
2024-09-22 21:05:52 -0500

    feat(Destacados): Incluye columna de destacados

--- resources/views/layouts/intranet.blade.php
+++ resources/views/layouts/intranet.blade.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Layout Básico con Tailwind</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="antialiased bg-gray-100" x-data="{ isMenuOpen: true }">
+<body class="font-sans antialiased bg-gray-100" x-data="{ isMenuOpen: true }">
     <div class="md:flex md:flex-col md:min-h-screen">
         <!-- Header -->
         <header class="flex items-center p-3 text-white bg-gray-800 md:flex-none">
@@ -37,9 +43,23 @@
             </nav>
 
             <!-- Contenido principal -->
-            <main class="flex-1 p-4 bg-white">
-                <h2 class="mb-3 text-lg font-semibold">Contenido Principal</h2>
-                <p>Aquí va el contenido de la página...</p>
+            <main class="flex-1">
+                <div class="h-full lg:flex">
+                    <!-- Contenido principal -->
+                    <div class="h-full p-2 lg:flex-1">
+                        <h2 class="mb-3 text-lg font-semibold">Contenido Principal</h2>
+                        <p>Aquí va el contenido de la página...</p>
+                    </div>
+                    <!-- Barra lateral de destacados -->
+                    <div class="hidden h-full p-2 lg:block lg:w-64 bg-slate-200">
+                        <h2 class="mb-3 text-lg font-semibold">Destacados</h2>
+                        <ul>
+                            <li>Destacado 1</li>
+                            <li>Destacado 2</li>
+                            <li>Destacado 3</li>
+                        </ul>
+                    </div>
+                </div>
             </main>
         </div>
 
@@ -48,6 +68,7 @@
             <p>© 2024 Mi Aplicación. Todos los derechos reservados.</p>
         </footer>
     </div>
+    @livewireScripts
 </body>
 
 </html>
