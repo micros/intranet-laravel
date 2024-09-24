b810e667e46d356312eb6e80c649edb050453619
Leonardo Forero Sandoval <gerencia@micrositios.net>
2024-09-23 22:26:54 -0500

    Mejora iconos

--- resources/views/components/intranet/layout/administration.blade.php
+++ resources/views/components/intranet/layout/administration.blade.php
@@ -1,6 +1,6 @@
 <div class="relative" x-data="{ adminOpen: false }">
     <button @click="adminOpen = !adminOpen"
-        class="flex items-center px-3 py-3 text-white bg-gray-700 hover:bg-gray-600 focus:outline-none">
+        class="flex items-center justify-center bg-gray-700 rounded text-whire w-9 h-9 hover:bg-gray-600 focus:outline-none">
         <i class="fas fa-cogs"></i>
     </button>
     <div x-show="adminOpen" @click.away="adminOpen = false"
--- resources/views/components/intranet/layout/toggle-menu.blade.php
+++ resources/views/components/intranet/layout/toggle-menu.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center flex-grow md:flex-grow-0">
     <button @click="isMenuOpen = !isMenuOpen"
-        class="flex items-center px-3 py-3 text-white bg-gray-700 hover:bg-gray-600 focus:outline-none">
+        class="flex items-center justify-center bg-gray-700 rounded text-whire w-9 h-9 hover:bg-gray-600 focus:outline-none">
         <i class="fas fa-bars"></i>
     </button>
 </div>
