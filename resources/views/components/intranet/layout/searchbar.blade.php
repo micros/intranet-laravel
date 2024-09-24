b5bb8b808fd44d8145796dc01b37425bd84b9bf8
Leonardo Forero Sandoval <gerencia@micrositios.net>
2024-09-24 00:03:23 -0500

    Mejora diseño de caja de busqueda

--- resources/views/components/intranet/layout/searchbar.blade.php
+++ resources/views/components/intranet/layout/searchbar.blade.php
@@ -1,7 +1,11 @@
-<input type="text" placeholder="Buscar..."
-    class="w-full px-3 py-2 text-gray-900 transition duration-200 ease-in-out bg-gray-200 border border-gray-300 rounded-l-md md:w-96 focus:outline-none focus:ring-2 focus:ring-orange-300 focus:bg-orange-50">
+<div class="relative w-full md:w-[28rem]">
+    <!-- Caja de búsqueda -->
+    <input type="text" placeholder="Buscar..."
+        class="w-full px-6 py-2 text-gray-900 transition duration-200 ease-in-out bg-gray-200 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-orange-300 focus:bg-orange-50">
 
-<!-- Botón de búsqueda -->
-<button class="px-3 py-2 bg-gray-600 hover:bg-gray-500 focus:outline-none rounded-r-md">
-    <i class="text-white fas fa-search"></i>
-</button>
+    <!-- Botón de búsqueda -->
+    <button
+        class="absolute flex items-center justify-center transform -translate-y-1/2 bg-gray-600 rounded-full right-2 top-1/2 hover:bg-gray-500 focus:outline-none w-9 h-9">
+        <i class="text-white fas fa-search"></i>
+    </button>
+</div>
