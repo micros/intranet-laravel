57776099e21a7c211a74cfe625f93899d4dd5379
Leonardo Forero Sandoval <gerencia@micrositios.net>
2024-09-23 21:44:04 -0500

    Ruta a mi perfil

--- resources/views/components/intranet/layout/profile.blade.php
+++ resources/views/components/intranet/layout/profile.blade.php
@@ -5,7 +5,7 @@
     </button>
     <div x-show="profileOpen" @click.away="profileOpen = false"
         class="absolute left-0 z-50 w-48 mt-2 bg-white rounded shadow-lg" x-transition.origin.top.right>
-        <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-100">Ver Perfil</a>
+        <a href="{{ route('profile') }}" class="block px-4 py-2 text-gray-800 hover:bg-gray-100">Ver Perfil</a>
         <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-100">Cerrar Sesión</a>
     </div>
 </div>
