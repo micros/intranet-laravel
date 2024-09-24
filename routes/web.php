fd8d7c38ba2f6db28c650ea2d511471526f2ec71
Leonardo Forero Sandoval <gerencia@micrositios.net>
2024-09-23 21:32:33 -0500

    Incluye profile

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home')->name('home');
-
-Route::view('/{slug}', 'default')->name('default');
 
 Route::prefix('admin')->group(function () {
 
@@ -18,3 +15,12 @@ Route::prefix('admin')->group(function () {
 
     require __DIR__ . '/auth.php';
 });
+
+
+Route::view('/', 'home')
+    ->middleware(['auth'])
+    ->name('home');
+
+Route::view('/{slug}', 'default')
+    ->middleware(['auth'])
+    ->name('default');
