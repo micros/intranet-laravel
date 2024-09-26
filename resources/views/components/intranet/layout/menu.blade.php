b6723ed3a0fdf2ac6b130bfe3c7f7d18e88d23e5
Leonardo Forero Sandoval <gerencia@micrositios.net>
2024-09-25 21:02:18 -0500

    Cambios menores en diseño

--- resources/views/components/intranet/layout/menu.blade.php
+++ resources/views/components/intranet/layout/menu.blade.php
@@ -3,7 +3,8 @@
     class="inset-0 z-40 flex flex-col justify-between text-white bg-slate-500 md:relative md:w-64 md:h-auto"
     -translate-x-full>
     <!-- Nombre de la aplicación -->
-    <h1 class="px-4 py-2 text-xl font-semibold text-orange-200">{{ config('app.name', 'Intranet Micrositios') }}</h1>
+    <h1 class="px-4 py-2 text-xl font-semibold text-orange-200 bg-slate-600">
+        {{ config('app.name', 'Intranet Micrositios') }}</h1>
 
     <ul class="mb-auto">
         <li><a href="/" class="block px-4 py-2 md:pl-8 hover:bg-blue-600">Inicio</a></li>
